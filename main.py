99dc850e8f239ddac701d718b8b461efbef65ef1
mark <marklca@hotmail.com>
2023-07-02 23:01:29 +1000

    variables and dictionary added

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 #Payroll app
+
+positions = []
+
+
+Level1 = 60000
+level2 = 65000
+level3 = 75000
+level4 = 90000
+level5 = 120000
+
+Worker1 = {
+    'name' : 'rob',
+    'position' : '()',
+    'branch' : 'Sydney' 
+}
