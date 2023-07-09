6eb5f7c4d6b9c753bbba9cdafd1b5577900634d0
mark <marklca@hotmail.com>
2023-07-09 17:15:45 +1000

    employee input code updated

--- classes.py
+++ classes.py
@@ -1,7 +1,8 @@
 # make minimum 6 functions
 
 class Employee:
-    def __init__(self, name, hourly_rate, hours_worked):
+    def __init__(self, name, position, paygrade, experience):
         self.name = name
-        self.hourly_rate = hourly_rate
-        self.hours_worked = hours_worked
+        self.position = position
+        self.paygrade = paygrade
+        self.experience = experience
--- employee.py
+++ employee.py
@@ -1,8 +1,30 @@
-employees = {
-    "1. Mark" : "Navarro",
-    "2. Adam" : "Lloyd",
-    "3. Ben" : "Chia",
-    "4. Scott" : "Daley",
-    "5. Mac" : "Dino",
-    
-}
+
+
+class employee_details:
+    def __init__(self, name, position, paygrade, experience):
+        self.name = name
+        self.position = position
+        self.paygrade = paygrade
+        self.experience = experience
+
+employees = {}
+
+name = input("Enter employee name: ")
+position = input("Enter employee position: ")
+paygrade = int(input("Enter employee paygrade level (0-6): "))
+experience = input("Enter employee experience type (junior/medior/senior): ")
+
+
+# Using employee details class
+employee = employee_details(name, position, paygrade, experience)
+
+# Add the employee to the employees dictionary
+employees[name] = employee
+
+# View added employee
+for name, employee in employees.items():
+    print("***** New Employee Added *****")
+    print(f"Employee Name: {employee.name}")
+    print(f"Position: {employee.position}")
+    print(f"Paygrade Level: {employee.paygrade}")
+    print(f"Experience: {employee.experience}")
